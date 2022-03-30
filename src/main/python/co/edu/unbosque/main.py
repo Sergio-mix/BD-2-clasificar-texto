ef55596032873711e5a725e6807e31603c8a6c77
alejo <alejo.80.123@gmail.com>
2022-03-30 09:43:37 -0500

    3

--- src/main/python/co/edu/unbosque/main.py
+++ src/main/python/co/edu/unbosque/main.py
@@ -9,15 +9,36 @@ datos = read.readExcel(
 )
 
 
-def calcular_Categoria(text):
-    list = ["mercado"]
+def returnProduct(text, list):
+    for product in list:
+        if product["text"] == text:
+            return product["return"]
+
+
+def calcular_Product(text):
+    list = [
+        {"text": "colegio", "return": "utiles"},
+        {"text": "smartphone", "return": "celular"},
+        {"text": "disney", "return": "peliculas"},
+        {"text": "películas", "return": "películas"},
+        {"text": "it", "return": "tecnología"},
+        {"text": "desarrolladores", "return": "tecnología"},
+        {"text": "navidad", "return": "regalos"}
+    ]
+
+    listProducts = ["colegio", "smartphone", "disney", "películas", "it", "desarrolladores", "navidad"]
+
     patron = re.compile(r'\W+')
-    palabras = patron.split(text)
-    print(palabras)
-    for palabra in palabras:
-        if list.count(palabra):
-            return palabra
-    return None
+    for palabra in patron.split(text):
+        if listProducts.count(palabra) == 1:
+            return returnProduct(palabra, list)
+    return "No se encontro"
 
 
-print(calcular_Categoria(datos[2]['text']))
+for dato in datos:
+    if dato["text"] == "":
+        datos.remove(dato)
+    else:
+        dato["product"] = calcular_Product(dato["text"])
+
+print(datos)
